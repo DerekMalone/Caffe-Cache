7279dbcfbfcb7984c52be203a20110880a879e51
DerekMalone <laxmalone@gmail.com>
2022-05-21 13:26:48 -0500

    pushing changes to Models that did not push the first time

--- Caffe-Cache/Models/Brew.cs
+++ Caffe-Cache/Models/Brew.cs
@@ -1,6 +1,18 @@
-﻿namespace Caffe_Cache.Models
+﻿
+
+namespace Caffe_Cache.Models
 {
     public class Brew
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string GrindSize { get; set; }
+        public int CoffeeWeight { get; set; }
+        public int WaterVolume { get; set; }
+        public TimeOnly BrewDuration { get; set; }
+        public string BrewInstructions { get; set; }
+        public string UserId { get; set; }
+        public int MachineId { get; set; }
+        public int CoffeeId { get; set; }
     }
 }
--- Caffe-Cache/Models/Coffee.cs
+++ Caffe-Cache/Models/Coffee.cs
@@ -2,5 +2,10 @@
 {
     public class Coffee
     {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public string Name { get; set; }
+        public string RoastType { get; set; }
+        public string UserID { get; set; }
     }
 }
