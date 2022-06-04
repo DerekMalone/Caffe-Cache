c8f71412006023f37dad8ac310ff7f7ab4202b42
DerekMalone <laxmalone@gmail.com>
2022-06-03 21:22:38 -0500

    added delete functionality to machines

--- caffe-cache-client/src/data/index.js
+++ caffe-cache-client/src/data/index.js
@@ -1,5 +1,5 @@
 // export { default as getCurrentUserUid } from './userHelper'; // Most likely wont Need.
-export { getMachinesByUid, getMachineById } from './machineData';
+export { getMachinesByUid, getMachineById, addMachine, editMachine, deleteMachine } from './machineData';
 export { default as coffeeData } from './coffeeData';
 export { default as brewData } from './brewData';
 
