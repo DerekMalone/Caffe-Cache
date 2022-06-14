0facb13035c35bf7cfbdba9c5e42eb0b9cc55c4d
DerekMalone <laxmalone@gmail.com>
2022-06-13 21:36:24 -0500

    home styling established

--- caffe-cache-client/src/views/HomeView.js
+++ caffe-cache-client/src/views/HomeView.js
@@ -1,36 +1,44 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom';
-import { Button } from 'reactstrap';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Button } from "reactstrap";
 
 export const HomeView = ({ userUID }) => {
   const navigate = useNavigate();
-  
+
   const handleNavMachineForm = () => {
-    navigate('/MachineForm');
-  }
+    navigate("/MachineForm");
+  };
 
   const handleNavCoffeeForm = () => {
-    navigate('/CoffeeForm');
-  }
+    navigate("/CoffeeForm");
+  };
 
   const handleNavBrewForm = () => {
-    navigate('/BrewForm');
-  }
+    navigate("/BrewForm");
+  };
 
   return (
     <>
-      <h1 className="view-heading">HomeView</h1>
-    <ul>
-      <li className='no-bullets add-btn'>        
-      <Button type='button' onClick={handleNavMachineForm}>Add New Machine</Button>
-      </li>
-      <li className='no-bullets add-btn'>        
-      <Button type='button' onClick={handleNavCoffeeForm}>Add New Coffee</Button>
-      </li>
-      <li className='no-bullets add-btn'>        
-      <Button type='button' onClick={handleNavBrewForm}>Add New Brew</Button>
-      </li>
-    </ul>
+      <div className="home-container">
+        <h1 className='view-heading'>Let's Get Brewin'</h1>
+        <ul>
+          <li className='no-bullets add-btn'>
+            <Button className='home-btn-style' type='button' onClick={handleNavMachineForm}>
+              Add New Machine
+            </Button>
+          </li>
+          <li className='no-bullets add-btn'>
+            <Button className='home-btn-style' type='button' onClick={handleNavCoffeeForm}>
+              Add New Coffee
+            </Button>
+          </li>
+          <li className='no-bullets add-btn'>
+            <Button className='home-btn-style' type='button' onClick={handleNavBrewForm}>
+              Add New Brew
+            </Button>
+          </li>
+        </ul>
+      </div>
     </>
-  )
-}
+  );
+};
